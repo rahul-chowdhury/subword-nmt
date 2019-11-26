902c48d87c0e7e43efa77a97e539af5f8b0724e3
ld-christian_hadiwinoto <ld-christian_hadiwinoto@hadoop.recommend.shopeemobile.com>
2019-11-26 11:07:15 +0800

    Split by space only

--- subword_nmt/learn_bpe.py
+++ subword_nmt/learn_bpe.py
@@ -71,7 +71,7 @@ def get_vocabulary(fobj, is_dict=False):
     for i, line in enumerate(fobj):
         if is_dict:
             try:
-                word, count = line.strip('\r\n ').split('\t ')
+                word, count = line.strip('\r\n ').split(' ')
             except:
                 print('Failed reading vocabulary file at line {0}: {1}'.format(i, line))
                 sys.exit(1)
